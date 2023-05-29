47719ead8df786115f2cabd75be4f7795e94d99a
harma <harma@10.48.49.112>
2023-05-29 13:13:33 -0400

    version 1

---
+++ src/week3/Week3.java
@@ -0,0 +1,20 @@
+
+
+package week3;
+
+/**
+ *This class +++ Insert class description here +++
+ *
+ * @author $ Harmanleen Chopra
+ */
+public class Week3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("learning github operations");
+    }
+
+}
